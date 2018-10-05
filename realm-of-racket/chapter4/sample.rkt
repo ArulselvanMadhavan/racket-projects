ff15857446220b895446402993149aced1cea899
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-10-04 21:47:50 -0700

    Finished upto chapter 4.5

--- realm-of-racket/chapter4/sample.rkt
+++ realm-of-racket/chapter4/sample.rkt
@@ -12,3 +12,27 @@
 (cond [(= x 7) 'equal]
       [(odd? x) 'odd]
       [else 'even])
+
+(struct student (name id# dorm) #:transparent)
+
+(define (my-length a-list)
+  (if (empty? a-list)
+      0
+      (add1 (my-length (rest a-list)))))
+
+(struct point (x y) #:transparent)
+
+(define (distance-to-origin p)
+  (sqrt (+ (sqr (point-x p)) (sqr (point-y p)))))
+
+(define pt1 (point -1 2))
+(define pt2 (point -1 2))
+
+(equal? pt1 pt2)
+
+(require rackunit)
+
+(check-equal? (add1 5) 6)
+;; (check-equal? (add1 5) 7)
+
+(check-equal? (sub1 (add1 5)) 5)
